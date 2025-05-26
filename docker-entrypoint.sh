798e8f801431f92bec5d4d5cf56f9aa6409b5652
Stef van Herk <547380@student.fontys.nl>
2025-05-26 11:34:14 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ ! -f /var/www/html/wp-config.php ]; then
     echo "Copying custom wp-config.php..."
-    cp wp-config.php /var/www/html/wp-config.php
+    cp /custom/wp-config.php /var/www/html/wp-config.php
 fi
 
 exec "$@"
