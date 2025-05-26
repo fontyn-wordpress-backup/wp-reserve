f0118b8b35ac3705b874b77917b34f77bced2d5c
Stef van Herk <547380@student.fontys.nl>
2025-05-26 10:45:14 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -23,15 +23,14 @@ if ($GLOBALS['ext_db_connection']->connect_error) {
     error_log('External DB connection failed: ' . $GLOBALS['ext_db_connection']->connect_error);
 }
 
-$salt_lines = getenv('WP_SALTS');
-if ($salt_lines) {
-    // Evaluate the lines so that define() statements run
-    eval($salt_lines);
-} else {
-    // Fallback - no salts defined (not recommended)
-    define('AUTH_KEY', 'put fallback here');
-    // ... define the rest similarly ...
-}
+define('AUTH_KEY', getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY', getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY', getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY', getenv('NONCE_KEY'));
+define('AUTH_SALT', getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT', getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT', getenv('NONCE_SALT'));
 
 define('WP_CACHE', true);
 define('WP_POST_REVISIONS', 5);
