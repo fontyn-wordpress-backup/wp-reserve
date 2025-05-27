5345bb3c05e0e32290ccc27acbea748892c4db04
Stef van Herk <547380@student.fontys.nl>
2025-05-27 09:25:12 +0200

    Delete docker-entrypoint.sh

--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ ! -f /var/www/html/wp-config.php ]; then
-    echo "Copying custom wp-config.php..."
-    cp /custom/wp-config.php /var/www/html/wp-config.php
-fi
-
-exec "$@"
