02c842d2746b8ace48bacb79e456ccb277f2e435
Stef van Herk <547380@student.fontys.nl>
2025-06-02 11:01:23 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,5 +1,5 @@
 <?php
-// Full WP Deploy Test with SSL
+// Full WP Deploy Test with SSL 2
 define('DB_NAME', getenv('WORDPRESS_DB_NAME') ?: 'wordpress_db');
 define('DB_USER', getenv('WORDPRESS_DB_USER') ?: 'wpuser');
 define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') ?: 'password');
