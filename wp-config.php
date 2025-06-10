233f27da7d78157bd8d34d6377ead7c2d0f37e2d
fontyn-wordpress-backup <github@fonteyn.cloud>
2025-06-10 10:24:40 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,9 +1,9 @@
 <?php
-
-define('DB_NAME', getenv('WORDPRESS_DB_NAME'));
+// 2
+define('DB_NAME', getenv('WORDPRESS_DB_NAME') ?: 'wordpress_db');
 define('DB_USER', getenv('WORDPRESS_DB_USER'));
 define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
-define('DB_HOST', getenv('WORDPRESS_DB_HOST'));
+define('DB_HOST', getenv('WORDPRESS_DB_HOST') ?: 'mysqlserver.mysql.database.azure.com');
 define('DB_CHARSET', 'utf8mb4');
 define('DB_COLLATE', '');
 
