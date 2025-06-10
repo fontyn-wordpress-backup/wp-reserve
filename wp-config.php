f0fdbd7fd5469b86d52253f41d59b7d6446e89c7
fontyn-wordpress-backup <github@fonteyn.cloud>
2025-06-10 09:52:11 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,15 +1,15 @@
 <?php
-// 2
-define('DB_NAME', getenv('WORDPRESS_DB_NAME') ?: 'wordpress_db');
-define('DB_USER', getenv('WORDPRESS_DB_USER') ?: 'wpuser');
-define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') ?: 'password');
-define('DB_HOST', getenv('WORDPRESS_DB_HOST') ?: 'mysqlserver.mysql.database.azure.com');
+
+define('DB_NAME', getenv('WORDPRESS_DB_NAME'));
+define('DB_USER', getenv('WORDPRESS_DB_USER'));
+define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
+define('DB_HOST', getenv('WORDPRESS_DB_HOST'));
 define('DB_CHARSET', 'utf8mb4');
 define('DB_COLLATE', '');
 
-define('EXT_DB_NAME', getenv('EXT_DB_NAME') ?: 'external_db');
-define('EXT_DB_USER', getenv('EXT_DB_USER') ?: 'wpuser');
-define('EXT_DB_PASSWORD', getenv('EXT_DB_PASSWORD') ?: 'password');
+define('EXT_DB_NAME', getenv('EXT_DB_NAME'));
+define('EXT_DB_USER', getenv('EXT_DB_USER'));
+define('EXT_DB_PASSWORD', getenv('EXT_DB_PASSWORD'));
 define('EXT_DB_HOST', DB_HOST); 
 
 define('WP_HOME','https://fonteyn.cloud');
